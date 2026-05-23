4ddacabe70c0efceb6cdc9b5d30c25168f08de1b
Meshan Fernando <meshan1236@gmail.com>
2026-05-23 23:01:21 +0200

    Rewrite chapter 01 with key themes, add \ac{} references, update settings

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -1,8 +1,3 @@
-# Close any Edge window that has main.pdf open (graceful close, not kill)
-Get-Process msedge -ErrorAction SilentlyContinue |
-    Where-Object { $_.MainWindowTitle -like "*main.pdf*" } |
-    ForEach-Object { $_.CloseMainWindow() | Out-Null }
-
 $paperDir = "$PSScriptRoot\..\Paper"
 
 Push-Location $paperDir
@@ -12,9 +7,6 @@ try {
     biber main
     pdflatex -interaction=nonstopmode main.tex
     pdflatex -interaction=nonstopmode main.tex
-
-    # Open compiled PDF in Edge
-    Start-Process "msedge" "$paperDir\main.pdf"
 }
 finally {
     Pop-Location
